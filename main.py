d6e8e292d8864c5934dca88e4d2aa39ba83ba08b
Pawel Pronobis <pronobispawe@gmail.com>
2025-01-09 12:26:55 +0100

    Zmiana w main i test_main - test nowego yml

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add(a, b):
-    return a + b
+def substr(a, b):
+    return a - b
 
 if __name__ == "__main__":
-    print("2 + 3 =", add(2, 3))
+    print("2 - 3 =", substr(2, 3))
 # Testowa zmiana
